9f18d68b0e4c74bdba870659a8bdd04c6dfefed6
lipen <felipe.anjos@unifil.br>
2026-03-10 14:04:44 -0300

    lipe

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <script setup>
-import MeuComponente from './components/MeuComponente.vue';
-import { ref } from 'vue';
-
-const valorMaximo = ref(12)
-const valorMaximoAtingido = ref(false)
-
-function onValorMaximo(atingiuValorMaximo) {
-  if(atingiuValorMaximo) {
-    valorMaximoAtingido.value = true
-  }
-}
+import TelaInicial from './views/TelaInicial.vue';
 
 </script>
 <template>
-  <h3 v-show="valorMaximoAtingido"> Valor máximo atingido </h3>
-  <MeuComponente :maximo="valorMaximo" @atingiu-valor-maximo="onValorMaximo"><!-- Parâmetros passados para as propriedades(props) do Componente customizado -->
-  </MeuComponente>
+  <TelaInicial></TelaInicial>  
 </template>
 
 <style scoped>
---
+++ src/views/TelaInicial.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="tela-inicial">
+        <h2> Tela inicial - Produtos </h2>
+        <div class="produtos">
+            <ProdutoItem class="produto-item" v-for="i in 10" :produto="produto"></ProdutoItem>
+        </div>
+
+    </div>
+</template>
+<script setup>
+import ProdutoItem from '@/components/produtos/ProdutoItem.vue';
+
+const produto = {
+    "nome": "Camiseta",
+    "descricao": "Camiseta preta de algodão",
+    "preco": 149.99,
+    "imgSrc": "@/assets/bone.jpeg"
+}
+
+</script>
+<style>
+
+.tela-inicial {
+    display: grid;
+    place-items: center; 
+    height: 100vh;
+}
+
+.produto-item {
+    margin: 20px;
+}
+
+</style>
