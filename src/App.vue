6e270f66ba9e24b6ebca6972db6c888e3b86964a
felipeanjosunifil <felipenatthan@gmail.com>
2026-03-10 20:55:36 -0300

    produtos inicio

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <script setup>
-import MeuComponente from './components/MeuComponente.vue';
 import { ref } from 'vue';
+import TelaInicial from './views/TelaInicial.vue';
 
-const valorMaximo = ref(5)
-const exibirAviso = ref(false)
-
-function onValorMaximo(atingiu){
-  exibirAviso.value = atingiu
-
-//  if(atingiu) {
-//    exibirAviso.value = true
-//  }
-
-}
 
 </script>
 <template>
-  <h2 v-show="exibirAviso"> O valor máximo foi atingido </h2>
-  <MeuComponente :valor-maximo="valorMaximo" @atingiu-valor-maximo="onValorMaximo">
-  </MeuComponente>
+  <TelaInicial></TelaInicial>
 </template>
 
 <style scoped>
---
+++ src/views/TelaInicial.vue
@@ -0,0 +1,55 @@
+<template>
+    <Header></Header>
+    <div class="tela-inicial">
+        <h2> Tela inicial - Produtos </h2>
+        <div class="produtos">
+            <ProdutoItem class="produto-item" v-for="p in produtos" :produto="p"></ProdutoItem>
+        </div>
+        <RouterLink to="/about"> Ir para página sobre </RouterLink>
+    </div>
+</template>
+<script setup>
+import Header from '@/components/Header.vue';
+import ProdutoItem from '@/components/produtos/ProdutoItem.vue';
+
+const produtos = [
+    {
+    "nome": "Camiseta",
+    "descricao": "Camiseta preta de algodão",
+    "preco": 149.99,
+    "imgSrc": "camiseta preta.webp"
+},
+{
+    "nome": "Bone",
+    "descricao": "UM bone legal",
+    "preco": 149.99,
+    "imgSrc": "bone.jpeg"
+},
+{
+    "nome": "tenis",
+    "descricao": "Tenis preto confortavel",
+    "preco": 149.99,
+    "imgSrc": "tenis.jpg"
+},
+{
+    "nome": "Placa de video",
+    "descricao": "Placa de video NVDIA RTX5090",
+    "preco": 50000.99,
+    "imgSrc": "rtx5090.jpeg"
+}
+]
+
+</script>
+<style>
+
+.tela-inicial {
+    display: grid;
+    place-items: center; 
+    height: 100vh;
+}
+
+.produto-item {
+    margin: 20px;
+}
+
+</style>
