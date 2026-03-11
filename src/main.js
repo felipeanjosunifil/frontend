5c85d00407e49a0b172ea6fb0e2ad8ced298514d
lipen <felipe.anjos@unifil.br>
2026-03-11 01:44:09 -0300

    navegação simples concluída.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import Header from './components/Header.vue'
 import router from './router/Router'
 
 const app = createApp(App)
-
-app.use(createPinia())
 
 app.component('Header', Header)
 app.use(router)
--- src/views/TelaInicial.vue
+++ src/views/TelaInicial.vue
@@ -2,54 +2,59 @@
     <Header></Header>
     <div class="tela-inicial">
         <h2> Tela inicial - Produtos </h2>
-        <div class="produtos">
-            <ProdutoItem class="produto-item" v-for="p in produtos" :produto="p"></ProdutoItem>
-        </div>
         <RouterLink to="/about"> Ir para página sobre </RouterLink>
+        <div class="produtos">
+            <ProdutoItem class="produto-item" v-for="p in produtos" :produto="p" @click="onItemClick"></ProdutoItem>
+        </div>
     </div>
 </template>
 <script setup>
 import Header from '@/components/Header.vue';
 import ProdutoItem from '@/components/produtos/ProdutoItem.vue';
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 const produtos = [
     {
-    "nome": "Camiseta",
-    "descricao": "Camiseta preta de algodão",
-    "preco": 149.99,
-    "imgSrc": "camiseta preta.webp"
-},
-{
-    "nome": "Bone",
-    "descricao": "Bné estiloso",
-    "preco": 80.99,
-    "imgSrc": "bone.jpeg"
-},
-{
-    "nome": "Tenis",
-    "descricao": "Tenis confortável",
-    "preco": 220.99,
-    "imgSrc": "tenis.jpg"
-},
-{
-    "nome": "Placa de vídeo",
-    "descricao": "Nvidia RTX5090",
-    "preco": 50000.99,
-    "imgSrc": "rtx5090.jpeg"
-}
+        "nome": "Camiseta",
+        "descricao": "Camiseta preta de algodão",
+        "preco": 149.99,
+        "imgSrc": "camiseta preta.webp"
+    },
+    {
+        "nome": "Bone",
+        "descricao": "Bné estiloso",
+        "preco": 80.99,
+        "imgSrc": "bone.jpeg"
+    },
+    {
+        "nome": "Tenis",
+        "descricao": "Tenis confortável",
+        "preco": 220.99,
+        "imgSrc": "tenis.jpg"
+    },
+    {
+        "nome": "Placa de vídeo",
+        "descricao": "Nvidia RTX5090",
+        "preco": 50000.99,
+        "imgSrc": "rtx5090.jpeg"
+    }
 ]
+
+function onItemClick() {
+    router.push('/about')
+}
 
 </script>
 <style>
-
 .tela-inicial {
     display: grid;
-    place-items: center; 
+    place-items: center;
     height: 100vh;
 }
 
 .produto-item {
     margin: 20px;
 }
-
 </style>
