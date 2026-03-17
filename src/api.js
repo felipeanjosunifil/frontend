05a810e55c3d9432331c030881b8ac50df92e37b
felipeanjosunifil <felipenatthan@gmail.com>
2026-03-17 18:38:14 -0300

    comentário jwt

--- src/api.js
+++ src/api.js
@@ -4,6 +4,7 @@ const apiConfiguration = axios.create({
     baseURL: "http://localhost:8080/api",
     headers: {
         'Content-Type': 'application/json',
+        //deve estar em um gerenciador de estado, pina ou pelo menos local storage, recebendo o header de forma dinâmica após o login do usuário.
         'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbkBlbWFpbC5jb20iLCJpYXQiOjE3NzM3ODI0ODQsImV4cCI6MTc3Mzc4NjA4NH0.gILH78KR7ZbepCmeyr4cdARoGunqQfIdcmAQlXThlXE'
     }
 })
