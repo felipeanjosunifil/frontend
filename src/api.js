0fab03cae51e3b3183395db1b40f9116b0099afa
felipeanjosunifil <felipenatthan@gmail.com>
2026-03-18 20:41:08 -0300

    miojo

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ const apiConfiguration = axios.create({
     headers: {
         'Content-Type': 'application/json',
         //deve estar em um gerenciador de estado, pina ou pelo menos local storage, recebendo o header de forma dinâmica após o login do usuário.
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbkBlbWFpbC5jb20iLCJpYXQiOjE3NzM3ODg1NzQsImV4cCI6MTc3Mzc5MjE3NH0.ygErh8UN49gUJAZ6VXS8WYuJOvbdqP98ttFBYmtJZgo'
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbkBlbWFpbC5jb20iLCJpYXQiOjE3NzM3OTM4MjIsImV4cCI6MTc3Mzc5NzQyMn0.xlWIGqOadIRZ-Wjq-Pp7_dRpzTEnfGsOhDN0bdCTuVQ'
     }
 })
 
--- src/views/PaginaInicialView.vue
+++ src/views/PaginaInicialView.vue
@@ -12,6 +12,7 @@
 import ProdutoItem from '@/components/produtos/ProdutoItem.vue';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import api from '@/api'
 
 const router = useRouter()
 
@@ -19,37 +20,19 @@ function onItemClick() {
     router.push('/about')
 }
 
-const produtos = ref([
-    {
-        "id": 1,
-        "nome": "Camiseta",
-        "descricao": "Camiseta preta de algodão",
-        "preco": 149.99,
-        "imgSrc": "camiseta preta.webp"
-    },
-    {
-        "id": 2,
-        "nome": "Bone",
-        "descricao": "Boné estiloso",
-        "preco": 80.99,
-        "imgSrc": "bone.jpeg"
-    },
-    {
-        "id": 3,
-        "nome": "Tenis",
-        "descricao": "Tenis confortável",
-        "preco": 220.99,
-        "imgSrc": "tenis.jpg"
-    },
-    {
-        "id": 4,
-        "nome": "Placa de vídeo",
-        "descricao": "Nvidia RTX5090",
-        "preco": 50000.99,
-        "imgSrc": "rtx5090.jpeg"
-    }
-])
+const produtos = ref([])
 
+api.get('/produtos')
+.then((response) => {
+    console.log(response.data)
+    produtos.value = response.data
+})
+.catch((error) => {
+    console.log(error)
+})
+.finally(() => {
+    console.log('executou o método GET /produtos')
+})
 
 </script>
 <style>
